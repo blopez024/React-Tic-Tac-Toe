37fc948ed0853f956d8d5f881a0e4aa09c5da936
blopez024 <bmlopez024@gmail.com>
2024-09-10 13:59:57 -0700

    Fix deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/'  // Replace with your GitHub repository name
+  base: '/react-tic-tac-toe/'
 });
