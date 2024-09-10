d836a70d3e1735a67d79a3e8bbba3012c6e06161
blopez024 <bmlopez024@gmail.com>
2024-09-10 13:53:48 -0700

    prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import ghPages from 'vite-plugin-gh-pages';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: '/react-tic-tac-toe/'  // Change this to your GitHub repository name
-
-})
+  plugins: [react()],
+  base: '/your-repo-name/'  // Replace with your GitHub repository name
+});
